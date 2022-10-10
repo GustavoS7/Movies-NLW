383f2e26946ca10c4ad2af9d3da78cc5de936849
Gustavo <gustavods2003@outlook.com>
2022-10-10 14:56:41 -0300

    dasdsa

--- src/assets/app/buildSlider.js
+++ src/assets/app/buildSlider.js
@@ -5,7 +5,7 @@ var buildMovies = ''
 const buildSlider = (array) => {
   for (let movie of array) {
     let movies = `
-    <a href="./movie.html?${movie.id}" target="blank">
+    <a href="./movie.html?id=${movie.id}" target="blank">
       <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="GTA V" class="postGames" id="${movie.id}">
     </a>
     `
